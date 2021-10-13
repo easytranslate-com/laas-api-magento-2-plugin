5a4214593e4b24b4eaa6d54006414e73c873773f
Simon Sprankel <simon@customgento.com>
2021-10-13 08:10:24 +0200

    Fix filters

--- src/Block/Adminhtml/Project/Tab/CmsPages.php
+++ src/Block/Adminhtml/Project/Tab/CmsPages.php
@@ -231,7 +231,7 @@ class CmsPages extends AbstractEntity
     /**
      * @SuppressWarnings(PHPMD.UnusedPrivateMethod)
      */
-    private function filterTranslatedCondition(CollectionData $collection, Column $column): void
+    protected function filterTranslatedCondition(CollectionData $collection, Column $column): void
     {
         $value = $column->getFilter()->getValue();
         if ($value) {
