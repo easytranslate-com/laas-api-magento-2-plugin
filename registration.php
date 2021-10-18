0554f412b7788f26485c19ad4ce8b84dfb6507a7
Simon Sprankel <simon@customgento.com>
2021-10-18 21:53:58 +0200

    Move registration.php again

--- registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'EasyTranslate_Connector',
-    __DIR__ . DIRECTORY_SEPARATOR . 'src'
-);
